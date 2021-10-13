f58c37cda7c6cde0a8bbb7bbcd339fd756603924
Zane Kozmak <59586892+zaxmo@users.noreply.github.com>
2021-10-13 09:46:38 -0600

    oct13

---
+++ WebAppRazorDemo/WebAppRazorDemo/Pages/BasicDataMovement.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebAppRazorDemo.Pages
+{
+    public class BasicDataMovementModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
